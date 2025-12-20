09a7abc275346e38eb1a259178aa4a6535d75cea
ismail <mmm.virtual.lab@gmail.com>
2025-12-21 00:06:00 +0530

    done some updated build

---
+++ intro/intro_data.js
@@ -0,0 +1,29 @@
+// js/intro_data.js
+
+export const INTRO_CONTENT = {
+    // Top Blue Bar Text
+    headerTitle: "Taper Angle Measurement ",
+    
+    // Button Text & Link
+    buttonLabel: "Go to Experiment",
+    buttonLink: "lab.html",
+
+    // Main Page Content
+    mainHeading: "Taper Angle Measurement ",
+    
+    // Aim Section
+    aimTitle: "Aim",
+    aimContent: "To measure the internal taper angle of a given workpiece using prcesion sphere",
+
+    // Acknowledgement Section
+    ackTitle: "Acknowledgement",
+    ackContent: "The Department of Production Technology appreciates the following students for their support in the development of Metrology and measurements virtual laboratory experiment.",
+    
+    // Student List
+    students: [
+        "A. M. ISMAIL - 2023507030",
+        "V. B. JAYARAM - 2023507040",
+        "M. RAM BHARATH - 2023507039",
+        "R. MONISH KUMAR - 2023507038"
+    ]
+};
